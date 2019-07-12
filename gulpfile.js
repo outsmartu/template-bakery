d59c21e66750759cf950e0bb74a09145021a4f7d
Artem Belyavskiy <artembell305@gmail.com>
2019-07-12 18:46:29 +0300

    working wathcing sass files

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,34 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const del = require('del');
+const { series, parallel } = gulp;
 sass.compiler = require('node-sass');
+
+const isDev = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
 gulp.task('sass', function () {
     return gulp.src('frontend/styles/main.sass')
-        .pipe(sass())
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('public/styles'));
+});
+
+gulp.task('clean', function(){
+    return del('public');
+});
+
+gulp.task('assets', function(){
+    return gulp.src('frontend/assets/**', {since: gulp.lastRun})
         .pipe(gulp.dest('public'));
 });
+
+gulp.task('build', series(
+    'clean', 
+    parallel('assets', 'sass')
+));
+
+gulp.watch('frontend/styles/**/*.*', series('sass'));
+gulp.watch('frontend/assets/**/*.*', series('assets'));
