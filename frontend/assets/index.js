83816020f708ee0090f62425137dc77b1fdfba2a
Artem Belyavskiy <artembell305@gmail.com>
2019-07-16 23:54:16 +0300

    added stupid script(facepalm)

---
+++ frontend/assets/index.js
@@ -0,0 +1,8 @@
+document.body.onmouseover = handler
+
+function handler(event){
+    const myli = event.target;
+    if( myli.tagName == 'li'){
+        const ai = document.querySelector()
+    }
+}
