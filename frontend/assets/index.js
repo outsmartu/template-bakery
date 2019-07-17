7d622d70494deff1ae18811b063f754e308da6cc
Artem Belyavskiy <artembell305@gmail.com>
2019-07-17 13:25:09 +0300

    working img status bar hovering

--- frontend/assets/index.js
+++ frontend/assets/index.js
@@ -1,8 +1,1 @@
-document.body.onmouseover = handler
-
-function handler(event){
-    const myli = event.target;
-    if( myli.tagName == 'li'){
-        const ai = document.querySelector()
-    }
-}
+// empty
