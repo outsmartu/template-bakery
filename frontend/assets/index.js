acb0c41363166989ac7e765faa6c49fa182a2aa7
Artem Belyavskiy <artembell305@gmail.com>
2019-07-25 21:17:47 +0300

    working on responsive fixed header animation

--- frontend/assets/index.js
+++ frontend/assets/index.js
@@ -31,20 +31,46 @@ function showSlide(index){
     slides.style.left = -currentOffset + '%';
 }
 
-let cross = false
+let collapsed = true
 function toggleHamburger(){
     
-    if (cross){
-        svg = document.getElementsByClassName('hamburger')[0]
-        svg.childNodes[1].setAttribute('transform', 'rotate(0)')
-        svg.childNodes[5].setAttribute('transform', 'rotate(0)')
-        svg.childNodes[3].style.display = ''
-    }else{
-        svg = document.getElementsByClassName('hamburger')[0]
-        svg.childNodes[1].setAttribute('transform', 'rotate(45)')
-        svg.childNodes[5].setAttribute('transform', 'rotate(-45)')
-        svg.childNodes[3].style.display = 'none'
+    let nav = document.getElementsByClassName('nav')[0]
+    let navSocials = nav.getElementsByClassName('nav__socials')[0]
+    if (!collapsed){
 
+        function makeCross(){
+            svg = document.getElementsByClassName('hamburger')[0]
+            svg.childNodes[1].setAttribute('transform', 'rotate(0)')
+            svg.childNodes[5].setAttribute('transform', 'rotate(0)')
+            svg.childNodes[3].style.display = ''
+        }
+
+        makeCross()
+
+        
+        nav.classList.remove('navbar-view_expanded')
+        nav.classList.add('navbar-view_collapsed')
+
+        // navSocials.classList.remove('nav__socials_expanded')
+        // navSocials.classList.add('nav__socials_collapsed')
+        // navSocials.style.display = 'flex'
+
+        
+    }else{
+        function makeBurger(){
+            svg = document.getElementsByClassName('hamburger')[0]
+            svg.childNodes[1].setAttribute('transform', 'rotate(45)')
+            svg.childNodes[5].setAttribute('transform', 'rotate(-45)')
+            svg.childNodes[3].style.display = 'none'
+        }
+        makeBurger()
+        
+        
+        nav.classList.remove('navbar-view_collapsed')
+        nav.classList.add('navbar-view_expanded')
+
+        // navSocials.classList.remove('nav__socials_collapsed')
+        // navSocials.classList.add('nav__socials_expanded')
     }
-    cross = !cross
+    collapsed = !collapsed
 }
