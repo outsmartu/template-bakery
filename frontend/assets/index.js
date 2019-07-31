ee9b0ba8b0292be9800906d46d3837c04c53d5ef
Artem Belyavskiy <artembell305@gmail.com>
2019-07-31 18:33:52 +0300

    cleared index.js, done responsive navbar

--- frontend/assets/index.js
+++ frontend/assets/index.js
@@ -1,88 +1,69 @@
-let currentSlide = 1;
-let n = 3;
-let offset = 100;
+let currentSlide = 1,
+    n = 3,
+    offset = 100
 
-showSlide(currentSlide);
-
-// var all_slides = document.body.getElementsByClassName('slides')[0];
-// const slides-collection = document.body.getElementsByClassName('slide');
+showSlide(currentSlide)
 
 function calcOffset(slideToShow) {
-    return (slideToShow - 1) * offset;
+    return (slideToShow - 1) * offset
 }
 
 function prev() {
-    // alert('show ' + (currentSlide - 1));
-    currentSlide = currentSlide == 1 ? n : --currentSlide;
-    showSlide(currentSlide);
-
+    currentSlide = currentSlide == 1 ? n : --currentSlide
+    showSlide(currentSlide)
 }
 
 function next() {
-    // alert('show ' + (currentSlide + 1));
-    currentSlide = currentSlide == n ? 1 : ++currentSlide;
-    showSlide(currentSlide);
+    currentSlide = currentSlide == n ? 1 : ++currentSlide
+    showSlide(currentSlide)
 }
 
 function showSlide(index) {
-    // alert(index);
-    let slides = document.body.getElementsByClassName('slides')[0];
-    let currentOffset = calcOffset(index)
-    slides.style.left = -currentOffset + '%';
+    let slides = document.body.getElementsByClassName('slides')[0],
+        currentOffset = calcOffset(index)
+    slides.style.left = -currentOffset + '%'
 }
 
 let collapsed = true
 function toggleHamburger() {
-
-    let nav = document.getElementsByClassName('nav')[0]
-    let navSocials = nav.getElementsByClassName('nav__socials')[0]
-
     let links = document.getElementsByClassName('links')[0]
     if (collapsed) {
+        function expandMenu() {
+            function makeCross() {
+                svg = document.getElementsByClassName('hamburger')[0]
+                svg.childNodes[1].setAttribute('transform', 'rotate(45)')
+                svg.childNodes[5].setAttribute('transform', 'rotate(-45)')
+                svg.childNodes[3].style.display = 'none'
+            }
 
-        function makeCross() {
-            svg = document.getElementsByClassName('hamburger')[0]
-            svg.childNodes[1].setAttribute('transform', 'rotate(45)')
-            svg.childNodes[5].setAttribute('transform', 'rotate(-45)')
-            svg.childNodes[3].style.display = 'none'
+            makeCross()
+            links.style.display = "flex"
         }
 
-        makeCross()
-
-
-        // nav.classList.remove('navbar-view_expanded')
-        // nav.classList.add('navbar-view_collapsed')
-
-        links.style.display = "flex"
-        // links.style.height = 'auto'
-
-
+        expandMenu()
     } else {
-        function makeBurger() {
-            svg = document.getElementsByClassName('hamburger')[0]
-            
-            svg.childNodes[1].setAttribute('transform', 'rotate(0)')
-            svg.childNodes[5].setAttribute('transform', 'rotate(0)')
-            svg.childNodes[3].style.display = ''
+        function collapseMenu() {
+            function makeBurger() {
+                svg = document.getElementsByClassName('hamburger')[0]
+
+                svg.childNodes[1].setAttribute('transform', 'rotate(0)')
+                svg.childNodes[5].setAttribute('transform', 'rotate(0)')
+                svg.childNodes[3].style.display = ''
+            }
+
+            makeBurger()
+            links.style.display = "none"
         }
-        makeBurger()
 
-
-        // nav.classList.remove('navbar-view_collapsed')
-        // nav.classList.add('navbar-view_expanded')
-
-        links.style.display = "none"
-        // links.style.height = 0
+        collapseMenu()
     }
     collapsed = !collapsed
 }
 
-// window.onscroll = function () {
-//     let scrolled = window.pageYOffset || document.documentElement.scrollTop;
-//     let nabvar = document.getElementsByTagName('nav')[0]
-//     if (scrolled >= 200){
-//         nabvar.style.top = 0
-//     }else{
-//         nabvar.style.top = `-${1.5 * nabvar.offsetHeight}px`
-//     }
-// }
+window.onscroll = function () {
+    let scrolled = window.pageYOffset || document.documentElement.scrollTop,
+        nabvar = document.getElementsByTagName('nav')[0],
+        scrollOffset = 300
+
+    scrolled >= scrollOffset ? nabvar.style.top = 0 : nabvar.style.top = `-${1.5 * nabvar.offsetHeight}px`
+}
