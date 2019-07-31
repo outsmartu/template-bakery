d8628c89f3bf5d8c118f5b3aaf1694e56b38a084
Artem Belyavskiy <artembell305@gmail.com>
2019-07-31 13:46:41 +0300

    added animation for navbar

--- docs/index.js
+++ docs/index.js
@@ -7,24 +7,24 @@ showSlide(currentSlide);
 // var all_slides = document.body.getElementsByClassName('slides')[0];
 // const slides-collection = document.body.getElementsByClassName('slide');
 
-function calcOffset(slideToShow){
+function calcOffset(slideToShow) {
     return (slideToShow - 1) * offset;
 }
 
-function prev(){
+function prev() {
     // alert('show ' + (currentSlide - 1));
     currentSlide = currentSlide == 1 ? n : --currentSlide;
     showSlide(currentSlide);
-    
+
 }
 
-function next(){
+function next() {
     // alert('show ' + (currentSlide + 1));
     currentSlide = currentSlide == n ? 1 : ++currentSlide;
     showSlide(currentSlide);
 }
 
-function showSlide(index){
+function showSlide(index) {
     // alert(index);
     let slides = document.body.getElementsByClassName('slides')[0];
     let currentOffset = calcOffset(index)
@@ -32,13 +32,13 @@ function showSlide(index){
 }
 
 let collapsed = true
-function toggleHamburger(){
-    
+function toggleHamburger() {
+
     let nav = document.getElementsByClassName('nav')[0]
     let navSocials = nav.getElementsByClassName('nav__socials')[0]
-    if (!collapsed){
+    if (!collapsed) {
 
-        function makeCross(){
+        function makeCross() {
             svg = document.getElementsByClassName('hamburger')[0]
             svg.childNodes[1].setAttribute('transform', 'rotate(0)')
             svg.childNodes[5].setAttribute('transform', 'rotate(0)')
@@ -47,7 +47,7 @@ function toggleHamburger(){
 
         makeCross()
 
-        
+
         nav.classList.remove('navbar-view_expanded')
         nav.classList.add('navbar-view_collapsed')
 
@@ -55,17 +55,17 @@ function toggleHamburger(){
         // navSocials.classList.add('nav__socials_collapsed')
         // navSocials.style.display = 'flex'
 
-        
-    }else{
-        function makeBurger(){
+
+    } else {
+        function makeBurger() {
             svg = document.getElementsByClassName('hamburger')[0]
             svg.childNodes[1].setAttribute('transform', 'rotate(45)')
             svg.childNodes[5].setAttribute('transform', 'rotate(-45)')
             svg.childNodes[3].style.display = 'none'
         }
         makeBurger()
-        
-        
+
+
         nav.classList.remove('navbar-view_collapsed')
         nav.classList.add('navbar-view_expanded')
 
@@ -73,4 +73,14 @@ function toggleHamburger(){
         // navSocials.classList.add('nav__socials_expanded')
     }
     collapsed = !collapsed
+}
+
+window.onscroll = function () {
+    let scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    let nabvar = document.getElementsByTagName('nav')[0]
+    if (scrolled >= 200){
+        nabvar.style.top = 0
+    }else{
+        nabvar.style.top = `-${1.5 * nabvar.offsetHeight}px`
+    }
 }
