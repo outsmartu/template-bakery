c64208745002e67209abbbd9a74da9165621cece
Artem Belyavskiy <artembell305@gmail.com>
2019-08-13 17:23:26 +0300

    added prod build

--- frontend/assets/index.js
+++ frontend/assets/index.js
@@ -1,4 +1,4 @@
-let currentSlide = 1,
+var currentSlide = 1,
     n = 3,
     offset = 100
 
@@ -9,24 +9,24 @@ function calcOffset(slideToShow) {
 }
 
 function prev() {
-    prevSlide = currentSlide == 1 ? n : currentSlide - 1
+    var prevSlide = currentSlide == 1 ? n : currentSlide - 1
     goToSlide(prevSlide)
 }
 
 function next() {
-    nextSlide = currentSlide == n ? 1 : currentSlide + 1
+    var nextSlide = currentSlide == n ? 1 : currentSlide + 1
     goToSlide(nextSlide)
 }
 
 function showSlide(index) {
-    let slides = document.body.getElementsByClassName('slides')[0],
+    var slides = document.body.getElementsByClassName('slides')[0],
         currentOffset = calcOffset(index)
     slides.style.left = -currentOffset + '%'
     currentSlide = index
 }
 
 function goToSlide(index) {
-    dashes = document.getElementsByClassName('slider__link')
+    var dashes = document.getElementsByClassName('slider__link')
 
     dashes[currentSlide - 1].classList.toggle('slider__link_active')
     dashes[index - 1].classList.toggle('slider__link_active')
@@ -34,13 +34,13 @@ function goToSlide(index) {
     showSlide(index)
 }
 
-let collapsed = true
+var collapsed = true
 function toggleHamburger() {
-    let links = document.getElementsByClassName('navbar__links')[0]
+    var links = document.getElementsByClassName('navbar__links')[0]
     if (collapsed) {
         function expandMenu() {
             function makeCross() {
-                svg = document.getElementsByClassName('hamburger')[0]
+                var svg = document.getElementsByClassName('hamburger')[0]
                 svg.childNodes[1].setAttribute('transform', 'rotate(45)')
                 svg.childNodes[5].setAttribute('transform', 'rotate(-45)')
                 svg.childNodes[3].style.display = 'none'
@@ -55,7 +55,7 @@ function toggleHamburger() {
     } else {
         function collapseMenu() {
             function makeBurger() {
-                svg = document.getElementsByClassName('hamburger')[0]
+                var svg = document.getElementsByClassName('hamburger')[0]
 
                 svg.childNodes[1].setAttribute('transform', 'rotate(0)')
                 svg.childNodes[5].setAttribute('transform', 'rotate(0)')
@@ -73,9 +73,9 @@ function toggleHamburger() {
 }
 
 window.onscroll = function () {
-    let scrolled = window.pageYOffset || document.documentElement.scrollTop,
+    var scrolled = window.pageYOffset || document.documentElement.scrollTop,
         nabvar = document.getElementsByTagName('nav')[0],
         scrollOffset = 300
 
-    scrolled >= scrollOffset ? nabvar.style.top = 0 : nabvar.style.top = `-${1.5 * nabvar.offsetHeight}px`
+    scrolled >= scrollOffset ? nabvar.style.top = 0 : nabvar.style.top = '-' + 1.5 * nabvar.offsetHeight + 'px'
 }
--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,10 @@ const gulp = require('gulp'),
     sourcemaps = require('gulp-sourcemaps'),
     del = require('del'),
     { series, parallel } = gulp,
-    browserSync = require('browser-sync').create()
+    browserSync = require('browser-sync').create(),
+    uglify = require('gulp-uglify'),
+    cleanCSS = require('gulp-clean-css'),
+    imagemin = require('gulp-imagemin')
 
 sass.compiler = require('node-sass')
 
@@ -52,5 +55,47 @@ function cleanProd() {
     return del(paths.prodDir)
 }
 
-exports.dev = series(series(clean, parallel(assets, processSass)), parallel(watch, enableBrowserSync))
-exports.prod = series(cleanProd)
+function minifyJS() {
+    return gulp.src('frontend/assets/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.prodDir))
+}
+
+function minifyCSS() {
+    return gulp.src('frontend/styles/main.sass')
+        .pipe(sass())
+        .pipe(cleanCSS())
+        .pipe(gulp.dest(paths.prodDir))
+}
+
+function minifyImages() {
+    return gulp.src('frontend/assets/images/*.*', {base: 'frontend/assets'})
+        .pipe(imagemin([
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.jpegtran({ progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+        ]))
+        .pipe(gulp.dest(paths.prodDir))
+}
+
+function copyFiles(){
+    return gulp.src('frontend/assets/**/*.html')
+        .pipe(gulp.dest(paths.prodDir))
+}
+
+exports.dev = series(
+    series(
+        clean,
+        parallel(
+            assets, processSass
+        )
+    ),
+    parallel(
+        watch, enableBrowserSync
+    ))
+
+exports.prod = series(
+    cleanProd, parallel(
+        minifyJS, minifyCSS, minifyImages, copyFiles
+    )
+)
